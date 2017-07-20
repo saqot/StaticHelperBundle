879bcf63f6b7ffb80f80b795bc4642b953e3951e
msh <mshirnin@treto.ru>
2017-07-20 17:56:43 +0700

    [+] oTranslator, oDoctrine, oEntityManager

--- SaqStaticHelperBundle.php
+++ SaqStaticHelperBundle.php
@@ -2,9 +2,13 @@
 
 namespace Saq\StaticHelperBundle;
 
+use Doctrine\Bundle\DoctrineBundle\Registry;
+use Doctrine\ORM\EntityManager;
 use Saq\StaticHelperBundle\Helper\ArraySaq;
 use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\DependencyInjection\Exception\InvalidArgumentException;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\Translation\IdentityTranslator;
 use Symfony\Component\VarDumper\VarDumper;
 
 
@@ -18,6 +22,9 @@ use Symfony\Component\VarDumper\VarDumper;
 class SaqStaticHelperBundle extends Bundle
 {
 	private static $oContainer;
+	private static $oTranslator;
+	private static $oDoctrine;
+	private static $oEntityManager;
 
 	public function setContainer(ContainerInterface $container = null)
 	{
@@ -31,6 +38,63 @@ class SaqStaticHelperBundle extends Bundle
 	public static function getContainer()
 	{
 		return self::$oContainer;
+	}
+
+	/**
+	 * Получение значений из параметров
+	 * @param string $name
+	 * @return mixed
+	 * @throws InvalidArgumentException
+	 */
+	public static function getParameter($name)
+	{
+		return self::getContainer()->getParameter($name);
+	}
+
+	/**
+	 * @return IdentityTranslator
+	 * @throws \LogicException
+	 */
+	public static function getTranslator()
+	{
+		if (!self::$oTranslator) {
+			if (!self::getContainer()->has('translator')) {
+				throw new \LogicException('translator not found');
+			}
+			self::$oTranslator = self::getContainer()->get('translator');
+		}
+		return self::$oTranslator;
+	}
+
+	/**
+	 * @return Registry
+	 * @throws \LogicException
+	 */
+	public static function getDoctrine()
+	{
+		if (!self::$oDoctrine) {
+			if (!self::getContainer()->has('doctrine')) {
+				throw new \LogicException('The DoctrineBundle is not registered');
+			}
+			self::$oDoctrine = self::getContainer()->get('doctrine');
+		}
+
+		return self::$oDoctrine;
+	}
+
+	/**
+	 * @param null $name
+	 * @return EntityManager
+	 */
+	public static function em($name = null)
+	{
+		$key = $name ? $name : 'default';
+
+		if (empty(self::$oEntityManager[$key])) {
+			self::$oEntityManager[$key] = self::getDoctrine()->getManager($name);
+		}
+
+		return self::$oEntityManager[$key];
 	}
 
 	/**
